eb07067c4996350fcebefb6c5691ad981d2bf6e4
Yanlin Gong <yanlin.gong@shroogle.com.au>
2019-11-06 12:47:45 +1100

    chore: footer componenet test

--- packages/ui-components/src/components/Elements/Footer/Footer.vue
+++ packages/ui-components/src/components/Elements/Footer/Footer.vue
@@ -1,7 +1,9 @@
 <template>
-  <el-container>
-      <el-footer>This is a footer</el-footer>
-  </el-container>
+  <footer class="footer">
+    <span>Shroogle Pty Ltd ABN 71 629 833 432 | Level 1, 91 William Street, Melbourne VIC 3000 Australia | Credit Representative of SJM Financial Solutions Pty Ltd t/as Resicom Pty Ltd | Australian Credit Licence Number 389191</span>
+    <br />
+    <span>Copyright © {new Date().getFullYear()} | All rights reserved</span>
+  </footer>
 </template>
 
 <script>
@@ -11,8 +13,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .el-footer {
-        background-color: blue;
-        bottom: 0
+@import '~@/assets/styles/variables.scss';
+    footer {
+        position: 'absolute';
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: '40px';
+        padding: 10px;
+        width: 100%;
+        font-size: 10px;
+        background-color: $brandColor;
+        color: $disabledColor;
+        text-align: center;
+        line-height: 12px;
     }
 </style>
