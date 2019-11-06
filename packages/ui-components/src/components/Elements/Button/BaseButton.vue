bfbe65d74674f933e1463c233c35cada0f3dceed
Yanlin Gong <yanlin.gong@shroogle.com.au>
2019-11-06 16:55:42 +1100

    chore: button test

---
+++ packages/ui-components/src/components/Elements/Button/BaseButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="onClick">
+      <slot>{{name}}</slot>
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'BaseButton',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        onClick: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/styles/variable.scss';
+    button {
+        font-size: 16px;
+        font-weight: 600;
+        padding-left: 27px;
+        padding-right: 27px;
+        max-width: 100%;
+        max-height: 100%;
+        margin: 0;
+        background-color: $secondaryColor;
+        &:focus {
+            outline: none;
+  }
+    }
+</style>
--- packages/ui-components/src/components/Elements/index.js
+++ packages/ui-components/src/components/Elements/index.js
@@ -1,2 +1,3 @@
 export { default as Footer } from './Footer/Footer.vue';
 export { default as Header } from './Header/Header.vue';
+export { default as BaseButton } from './Button/BaseButton.vue';
