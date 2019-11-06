f29909e2ea95dbb88d2a74cbbdf00806d29eaca6
Yanlin Gong <yanlin.gong@shroogle.com.au>
2019-11-06 13:05:50 +1100

    chore: change the file name

--- packages/ui-components/src/components/Elements/Footer/Footer.vue
+++ packages/ui-components/src/components/Elements/Footer/Footer.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~@/assets/styles/variables.scss';
+@import '~@/assets/styles/variable.scss';
     footer {
         position: 'absolute';
         bottom: 0;
