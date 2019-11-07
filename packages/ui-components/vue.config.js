f6c240e6f5435cc416074ef8b855143d5b023bf9
Yanlin Gong <yanlin.gong@shroogle.com.au>
2019-11-07 14:18:20 +1100

    chore: clear the project

--- packages/ui-components/vue.config.js
+++ packages/ui-components/vue.config.js
@@ -1,14 +1,13 @@
 // vue.config.js
 module.exports = {
-    chainWebpack: (config) => {
-      const svgRule = config.module.rule('svg');
-      svgRule.uses.clear();
-      svgRule
-        .use('vue-svg-loader')
-        .loader('vue-svg-loader');
-    },
-    css: {
-      extract: false
-    },
-  };
-  
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+  css: {
+    extract: false
+  }
+};
